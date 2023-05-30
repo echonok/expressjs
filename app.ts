347d0b17cb97e588853a824fd8672bbbfa2e360f
Nick Elobsky <echonok@gmail.com>
2023-05-30 21:11:39 +0200

    init project

--- app.ts
+++ app.ts
@@ -1,10 +1,20 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app: Express = express();
+const app = express();
 const port = process.env.PORT;
+
+app.use((req, res, next) => {
+  console.log('middleware 1')
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('middleware 2')
+  res.send('<h1>123</h1>');
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
